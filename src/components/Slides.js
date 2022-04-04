1d29a36fde95d3c1532857cebaf7b702e2c9eeb3
Asega Debele Ismail <asega03@gmail.com>
2022-04-04 16:30:57 +0300

    Declaring the slide position state and the restart, next and previous button slide postion on a click of each

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Slides({slides}) {
 
+    const slideLength = slides.length;
+
+    // Delaring state-slide positioning inorder to keep track of changes
+    const [slidePostion , setSlidePosition] = useState(0);
+
+    // Declaring inital slide position once the restart button is clicked
+    const restartButton = () => {
+        setSlidePosition(0)
+    }
+
+    // Declaring next slide navigation an a click on the next button
+    const nextButton = () => {
+        if (slidePostion === length - 1 ) {
+            return setSlidePosition(slidePostion + 1)
+        }
+    }
+
+    // Declaring previous slide navigation an a click on the previous button
+    const previousButton = () => {
+        if (slidePostion === length - 1 ) {
+            return setSlidePosition(slidePostion - 1)
+        }
+    }
+ 
     return (
         <div>
             <div id="navigation" className="text-center">
