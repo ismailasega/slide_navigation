003555485de6aabfe126bbe6a9d0ef2376e284de
Asega Debele Ismail <asega03@gmail.com>
2022-04-04 16:44:29 +0300

    Slide Navigation Set

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -10,32 +10,28 @@ function Slides({slides}) {
     // Declaring inital slide position once the restart button is clicked
     const restartButton = () => {
         setSlidePosition(0)
-    }
+    };
 
     // Declaring next slide navigation an a click on the next button
     const nextButton = () => {
-        if (slidePostion === length - 1 ) {
-            return setSlidePosition(slidePostion + 1)
-        }
-    }
+        setSlidePosition(slidePostion + 1)
+    };
 
     // Declaring previous slide navigation an a click on the previous button
     const previousButton = () => {
-        if (slidePostion === length - 1 ) {
-            return setSlidePosition(slidePostion - 1)
-        }
+        setSlidePosition(slidePostion - 1)
     }
  
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={restartButton} disabled={slidePostion === 0}>Restart</button>
+                <button data-testid="button-prev" className="small" onClick={previousButton} disabled={slidePostion === 0}>Prev</button>
+                <button data-testid="button-next" className="small"onClick={nextButton} disabled={slidePostion === slideLength - 1} >Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides[slidePostion]?.title}</h1>
+                <p data-testid="text">{slides[slidePostion]?.text}</p>
             </div>
         </div>
     );
